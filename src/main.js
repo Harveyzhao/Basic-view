c4cb3f2134a4d71a84ebb898d7a9bb1a27395a5e
Harvey Zhao <zhaoyh294@163.com>
2017-07-05 10:39:58 +0800

    update

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import '@/components/style/index'
 import App from './App'
 import router from './router'
-import './assets/style/index'
 import './assets/main.less'
 
 Vue.config.productionTip = false
